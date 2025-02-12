011c2ff24a3dea9a895c9266ed5c7563b4218529
Reginaldo L. Silva <reginaldoluiz75@gmail.com>
2025-02-12 20:42:01 -0300

    a2_funcao_print.py

---
+++ a2_funcao_print.py
@@ -0,0 +1,12 @@
+# print irá imprimir o resultado no terminal
+# sep="" é um separador
+# end="" é o final do print()
+# \r\n -> Carriage Return + Line Feed(CRLF)
+# \n -> Line Feed (LF) Unix
+# O Python diferencia letras maiúsculas de minúsculas
+# Sempre ler os erros do Python
+print(10, 20 ,1000, sep="-", end="#")
+print(5, 10 , 500, sep='*', end="\n")
+print(5, 10 , 500, sep='*', end="\n")
+
+#Print() com letra maiúscula vai gerar um erro
