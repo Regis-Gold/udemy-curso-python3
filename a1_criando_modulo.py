5168d1edd6e78d4cdc8b7c23a802a35e94f75624
Reginaldo L. Silva <reginaldoluiz75@gmail.com>
2025-02-12 20:39:23 -0300

    a1 criando modulos

---
+++ a1_criando_modulo.py
@@ -0,0 +1,22 @@
+# Este é o único tipo de comentário do Python
+
+"""
+DocString: não é um tipo de comentário, mas é muito usado como comentário
+"""
+
+# print irá imprimir o resultado no terminal
+print("")
+print('1-', 100 + 50); # adição
+print('2-', 100 - 50); # subtração
+print('3-', 100 * 50); # multiplicação
+print('4-', 100 / 50); # divisão
+print('5-', 100 ** 2); # elevado
+print('6-', 100 % 50); # módulo
+print('7-', 100 < 50); # maior que
+print('8-', 100 > 50); # menor que
+# print('9-', 100 === 50); # igual (equivalência)
+print('10-', 100 <= 50); # menor ou igual
+print('11-', 100 >= 50); # maior ou igual
+print('12-', 100 != 50); # diferente de
+# print(13-', 100 && 50); # operador lógico AND
+# print('14-', 100 || 50); # operador lógico OR
