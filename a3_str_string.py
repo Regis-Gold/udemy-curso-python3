812c4ef0d93c1b4875beaf90493469f6a9f617ce
Reginaldo L. Silva <reginaldoluiz75@gmail.com>
2025-02-12 20:43:30 -0300

    a3_str_string.py

---
+++ a3_str_string.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tiapgem (o tipo pode ser atribuido dinamicamente pelo Python) = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print(1, 'Regis Silva')
+print(2, "'Regis Silva'")
+print(3, '"Regis Silva"')
+print('Explicito', 'é', 'melhor " do que implicito')
+
+# Aspas duplas
+print(4, "Regis Silva")
+
+# Escape
+print(5, "Regis \"Silva\"")
+
+# r
+print(6, r"Regis \"Silva\"")
