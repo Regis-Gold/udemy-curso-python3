b1c31abf918e4c437709e36f69995183b73852ee
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-14 18:16:11 -0300

    Operadores de concatenação e repetição

---
+++ a10_conc_repeat.py
@@ -0,0 +1,10 @@
+concatenacao1 = 'Regis' + ' ' + 'Silva'
+concatenacao2 = 'A' + 'B' + 'C' + '1'
+print(concatenacao1)
+print(concatenacao2)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_nome = 3 * concatenacao1
+print(a_dez_vezes)
+print(tres_vezes_nome)
+print('*********************************')
