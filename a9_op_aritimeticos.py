6315fd8d95b7d7af7a566010b9c9b278b8ffadb2
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-14 18:15:10 -0300

    Operadores aritméticos mais usados em Python

---
+++ a9_op_aritimeticos.py
@@ -0,0 +1,29 @@
+# print irá imprimir o resultado no terminal
+print("")
+print('1-', 100 + 50); # adição
+print('2-', 100 - 50); # subtração
+print('3-', 100 * 50); # multiplicação
+print('4-', 100 / 50); # divisão (float)
+print('5-', 100 // 50); # divisão inteira (sem casas decimais em alguns casos)
+print('6-', 100 ** 2); # elevado
+print('7-', 105 % 10); # módulo (resto da divisão)
+
+
+print('8-', 100 > 50); # maior que
+print('9-', 100 < 50); # menor que
+print('10-', 100 == 100); # igual equivalência
+print('12-', 100 >= 50); # maior ou igual
+print('11-', 100 <= 50); # menor ou igual
+print('13-', 100 != 50); # diferente de
+print('14-', 100 and 50); # operador lógico and(e)
+print('15-', 100 or 50); # operador lógico or (ou)
+
+# Ver se um número é multiplo por outro
+print('***********************************')
+print(10 % 5 == 0)
+print(16 % 4 == 0)
+
+# Ver se o número é par ou ímpar
+print('***********************************')
+print(15 % 2 == 0)
+print(13 % 2 == 0)
