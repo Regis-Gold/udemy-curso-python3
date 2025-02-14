759ab6612a4591f54a6e900daf0b7d3848cb80e0
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-14 18:14:30 -0300

    Uso de variáveis em Python

---
+++ a8_ex_variaveis.py
@@ -0,0 +1,13 @@
+nome = 'Regis'
+sobrenome = 'Silva'
+idade = 25
+ano_nasc = 1998
+maioridade = bool(2023 - ano_nasc)
+altura_metros = 1.75
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nasc)
+print('É maior de idade?:', maioridade)
+print('Altura em metros:', altura_metros)
