29df3a6fad9fbd46017a5dc12da34959247a731f
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-15 21:55:23 -0300

    Criando uma calculadora de imc

---
+++ a_12_calc_imc.py
@@ -0,0 +1,17 @@
+# nome = "Regis Silva"
+# altura = 1.75
+# peso = 64
+# imc = peso / altura ** 2
+# print(nome, 'tem', altura, 'de altura')
+# print('pesa', peso, 'quilos e seu IMC é:')
+# print(imc)
+# print(...) # Pra não gerar erro no código enquanto você está digitando
+
+nome = input('Digite seu nome: ')
+altura = input('Digite sua altura separado por .')
+peso = input('Digite seu peso: ')
+imc = peso / altura ** 2
+print(nome, 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu IMC é:')
+print(imc)
+print(...) # Pra não gerar erro no código enquanto você está digitando
