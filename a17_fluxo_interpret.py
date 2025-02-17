7cf5770c001aa7b10589f12d849e25837b5d16a3
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-16 22:50:27 -0300

    Como funciona o fluxo do interpretador!

---
+++ a17_fluxo_interpret.py
@@ -0,0 +1,29 @@
+# if / elif      / else
+# se / se não se / se não
+
+cond1 = False
+cond2 = False
+cond3 = False
+cond4 = True
+
+if cond1:
+    print('Código para cond1')
+elif cond2:
+    print('Código para cond2')
+    print('Código para cond2')
+elif cond3:
+    print('Código para cond3')
+    print('Código para cond3')
+    print('Código para cond3')
+elif cond4:
+    print('Código para cond4')
+    print('Código para cond4')
+    print('Código para cond4')
+    print('Código para cond4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 20 == 20:
+    print('Outro if aleatório')
+    
+print('print fora do bloco de código')
