2bc0f9b1e8b08641a80d3d8973761b99fbf3729c
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-17 22:11:00 -0300

    Uso de operadores relacionais

---
+++ a19_op_relacionais.py
@@ -0,0 +1,20 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+#` !=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2>=2
+menor = 1 < 2
+menor_ou_igual = 2<=2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('Caminho PS E: > UDEMY > Python_3 > s3_logica_basica > python -i r_operadores_relacionais.py')
+print('../python -i r_operadores_relacionais.py')
+print('Abrir no Terminal Integrado')
