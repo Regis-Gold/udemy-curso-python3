e1f096997cada25f1033100e02c51bc6e4ef1137
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-22 19:15:37 -0300

    Usando loop while break 3

---
+++ a38_while_break3.py
@@ -0,0 +1,39 @@
+"""
+Operadores de atribuição com while
+=   +=   -=   *=   /=   //=   **=   %=
+"""
+
+contador = 1
+
+while contador <= 10:
+    print(f'{contador}º volta do loop')
+    contador += 1
+print('"AQUI ACABA O LAÇO"')
+
+
+
+contador1 = 10
+contador2 = 10
+contador3 = 10
+contador4 = 10
+contador5 = 10
+contador6 = 10
+contador7 = 10
+contador8 = 10
+
+contador1 = 10
+print(contador1)
+contador2 += 10
+print(contador2)
+contador3 -= 10
+print(contador3)
+contador4 *= 10
+print(contador4)
+contador5 /= 10
+print(contador5)
+contador6 //= 10
+print(contador6)
+contador7 **= 10
+print(contador7)
+contador8 %= 10
+print(contador8)
