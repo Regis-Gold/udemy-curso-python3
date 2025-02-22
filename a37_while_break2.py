0e7e3d3a25d7f66b79b7a3ddbabd0563d5403526
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-02-22 19:13:45 -0300

    Usando loop while break 2

---
+++ a37_while_break2.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+contador = 1
+
+while contador <= 10:
+    print(f'{contador}º volta do loop')
+    contador = contador + 1
+print('"AQUI ACABA O LAÇO"')
