28291f84bb0b68bfbf6da09a876f7f60d4ae0b8a
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-11 21:02:35 -0300

    Usando métodos úteis em Python

---
+++ a82_metodos_uteis_dict.py
@@ -0,0 +1,34 @@
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+
+pessoa = {
+    'nome': 'Reginaldo',
+    'sobrenome': 'Silva',
+    'idade': 27,
+}
+
+pessoa.setdefault('idade', 0)
+print(pessoa['idade'])
+
+print(len(pessoa))
+print(list(pessoa.keys()))
+print(list(pessoa.values()))
+print(list(pessoa.items()), f"\n")
+
+for valor in pessoa.values():
+    print(valor)
+print(f'\n')
+
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
+print(f'\n')
